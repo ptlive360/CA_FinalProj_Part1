b51e413bd7eeb6bb38a2c1e2cd5669243b752dcc
YC_Chang <ptlive360@gmail.com>
2017-10-22 21:56:54 +0800

    clean the code

--- CNNConvLayer.cu
+++ CNNConvLayer.cu
@@ -1,6 +1,3 @@
-//Show mercy to me!
-
-
 // This program executes a typical convolutional layer in regular CNNs.Neuron sparsity(zero ratio) is 50% and Weight sparsity is 70%.
 #include <iostream>
 #include "CNNConvLayer.h"
@@ -142,66 +139,10 @@ void convLayerGPU(int* filt_GPU, int* inNeu_GPU, int* out_GPU_kernel, int* out_N
 					}
 					outIdx = sli*outArea + fmy*FMSIZE/3 + fmx;
 					out_GPU_kernel[outIdx] = max;
-					//printf("%d\n", outNeuIdx);
 				}
 			}
 		}
 	}
-	/*
-	// Convolution
-	for(fn = 0; fn < FILTNUM; fn++){					//iterate through each filters
-		for(fmy = 0; fmy < FMSIZE; fmy += STRIDE){		//Stride through
-			for(fmx = 0; fmx < FMSIZE; fmx += STRIDE){	//Stride through
-				sum = 0;
-				for(sli = 0; sli < FMDEPTH; sli++){		//Iterate through depth
-					//Convolution
-					for(y = 0; y < FILTSIZE; y++){
-						for(x = 0; x < FILTSIZE; x++){
-							
-							ifmy = fmy - FILTSIZE / 2 + y;	
-							ifmx = fmx - FILTSIZE / 2 + x;
-							filtIdx = fn*filtVol + sli*filtArea + y*FILTSIZE + x;
-							inNeuIdx = sli*fmArea + ifmy*FMSIZE + ifmx;
-							if(ifmy >= 0 && ifmy < FMSIZE && ifmx >= 0 && ifmx < FMSIZE)
-								sum += filt_GPU[filtIdx] * inNeu_GPU[inNeuIdx];
-						}
-					}
-				}
-
-				// Activation - ReLU <- Don't pronounce it wrong
-				outNeuIdx = fn*fmArea + fmy*FMSIZE + fmx;
-				if(sum <= 0)
-					out_Neu_kernel[outNeuIdx] = 0;
-				else
-					out_Neu_kernel[outNeuIdx] = sum;
-				
-			}
-		}
-	}
-	
-	// Max Pooling with Window Size 3x3 and stride 3
-	int max, tmpVal;
-	for(sli = 0; sli < FILTNUM; sli++){
-		for(fmy = 0; fmy < FMSIZE/3 ; fmy += 1){
-			for(fmx = 0; fmx < FMSIZE/3 ; fmx += 1){
-				outNeuIdx = sli*fmArea + fmy*3*FMSIZE + fmx*3;
-				max = out_Neu_kernel[outNeuIdx];
-				for(y = 0; y < 3; y++){
-					for(x = 0; x < 3; x++){
-						ofmy = fmy*3 + y;
-						ofmx = fmx*3 + x;
-						outNeuIdx = sli*fmArea + ofmy*FMSIZE + ofmx;
-						tmpVal = out_Neu_kernel[outNeuIdx];	
-						if(tmpVal > max)
-							max = tmpVal;
-					}
-				}
-				outIdx = sli*outArea + fmy*FMSIZE/3 + fmx;
-				out_GPU_kernel[outIdx] = max;
-			}
-		}
-	}
-	*/
 }
 /***	Implement your CUDA Kernel here	***/
 
@@ -212,8 +153,6 @@ int main()
 	int convLayerCPUExecTime, convLayerGPUExecTime;
 	init();
 	
-
-
 	/******** Added ********/
 	int* filt_GPU;
 	int* inNeu_GPU;
@@ -241,16 +180,13 @@ int main()
   
 	//Convolution by GPU   
 	clock_gettime(CLOCK_REALTIME, &time_begin);
-	/***	Lunch your CUDA Kernel here	***/
-	/***	Lunch your CUDA Kernel here	***/
-	/***	Lunch your CUDA Kernel here	***/
 
+	/***	Lunch your CUDA Kernel here	***/
 	convLayerGPU<<<(FILTNUM*FMSIZE*FMSIZE+1023)/1024,1024>>>(filt_GPU, inNeu_GPU, out_GPU_kernel,  out_Neu_kernel); // Lunch the kernel
 	cudaDeviceSynchronize(); // Do synchronization before clock_gettime()
 	cudaMemcpy(outGPU, out_GPU_kernel, FILTNUM * FMSIZE/3 * FMSIZE/3*sizeof(int), cudaMemcpyDeviceToHost);
 	/***	Lunch your CUDA Kernel here	***/
-	/***	Lunch your CUDA Kernel here	***/
-	/***	Lunch your CUDA Kernel here	***/
+
 	clock_gettime(CLOCK_REALTIME, &time_end);
 	convLayerGPUExecTime = timespec_diff_us(time_begin, time_end);
 	cout << "GPU time for executing a typical convolutional layer = "  << ((float)convLayerGPUExecTime)/1000 << "ms" << endl;
